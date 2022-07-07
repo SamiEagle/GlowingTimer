520b7538e749be9bfa29d695b3188c50f2c29a5a
SamiEagle <67840664+SamiEagle@users.noreply.github.com>
2022-07-07 05:05:26 +0200

    Add files via upload

---
+++ timer/app.js
@@ -0,0 +1,31 @@
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+
+let counterSec=0;
+let start= ()=>{
+    let counterMin= prompt("enter a number from 1 to 60");
+    if(counterMin>60 || counterMin<1) {
+    alert("I said between 1 to 60 -_-")
+    return;}
+    min.innerHTML=counterMin;
+ let randoName =   setInterval(()=>{
+        if(counterSec === 0){
+        counterMin--;
+        min.innerHTML= counterMin.toString();
+        counterSec = 59
+    }
+        if(counterSec<10){ sec.innerHTML= "0"+counterSec.toString();}else{
+        sec.innerHTML= counterSec.toString();
+        }
+        counterSec--;
+        if(counterMin ==0 && counterSec ==0){
+            sec.innerHTML="00"
+            console.log("timer is done")
+            if(sec.innerHTML ="00"){ clearInterval(randoName);
+                setTimeout(()=>{
+                    alert("Timer is Done!")
+                }, 100)
+            }
+            }
+    },1000)
+}
